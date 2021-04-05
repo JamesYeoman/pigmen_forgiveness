c3288b5ef9b2520aa91a551079a64e032aa0e88e
James Yeoman <jamesyeoman10@gmail.com>
2021-04-05 15:02:52 +0100

    Apply several changes to the mixin class
    1. Extend Entity instead of EntityZombie (least common denominator)
    2. Simplify the inject method string
    3. Add logging and messages as a way to monitor
    if and when the becomeAngryAt mixin method is executed

--- src/main/java/com/degenerate_human/pigmen_forgiveness/mixin/MixinEntityPigZombie.java
+++ src/main/java/com/degenerate_human/pigmen_forgiveness/mixin/MixinEntityPigZombie.java
@@ -1,12 +1,13 @@
 package com.degenerate_human.pigmen_forgiveness.mixin;
 
+import com.degenerate_human.pigmen_forgiveness.PigmenForgiveness;
 import com.degenerate_human.pigmen_forgiveness.events.AngryAtPlayerEvent;
 import com.degenerate_human.pigmen_forgiveness.interfaces.ICanForgive;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityPigZombie;
-import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,7 +20,7 @@ import javax.annotation.Nullable;
 import java.util.UUID;
 
 @Mixin(EntityPigZombie.class)
-public abstract class MixinEntityPigZombie extends EntityZombie implements ICanForgive {
+public abstract class MixinEntityPigZombie extends Entity implements ICanForgive {
 
     @Shadow
     private int angerLevel;
@@ -44,10 +45,12 @@ public abstract class MixinEntityPigZombie extends EntityZombie implements ICanF
         this.setRevengeTarget(null);
     }
 
-    @Inject(at = @At("HEAD"), method = "becomeAngryAt(Lnet.minecraft.entity.Entity)V")
+    @Inject(at = @At("HEAD"), method = "becomeAngryAt")
     private void becomeAngryAt(Entity p_70835_1_, CallbackInfo callbackInfo) {
         if (p_70835_1_ instanceof EntityPlayer) {
             MinecraftForge.EVENT_BUS.post(new AngryAtPlayerEvent(this.getEntityId(), p_70835_1_.getUniqueID()));
+            PigmenForgiveness.LOGGER.info("One angery boi");
+            p_70835_1_.sendMessage(new TextComponentString("One angery boi"));
         }
     }
 }
